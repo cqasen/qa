f94172a0a7f64e59a720c5dd9a01e0e3d2675ad2
yisen <dengyinsen@gutou.com>
2024-01-19 11:06:22 +0800

    模型下载调整

--- config.py
+++ config.py
@@ -11,6 +11,9 @@ class Config:
     """
 
     embedding_model_name = 'BAAI/bge-small-zh-v1.5'  # 嵌入模型名称
-    pipeline_model_names = ['hfl/chinese-pert-large-mrc', 'wptoux/albert-chinese-large-qa']  # 流水线模型名称列表
+    pipeline_model_names = [
+        # 'hfl/chinese-pert-large-mrc',
+        # 'wptoux/albert-chinese-large-qa'
+    ]  # 流水线模型名称列表
     vector_store_path = 'vector_store_path'  # 向量存储路径
     dashscope_api_key = os.getenv(key='DASHSCOPE_API_KEY', default=None)  # Dashscope API Key
